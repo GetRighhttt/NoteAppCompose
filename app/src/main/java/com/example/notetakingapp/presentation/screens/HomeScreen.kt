fd34e50a25f6f46b96b6e3173444d718c1ee09e3
stefan bayne <stefanbayne@gmail.com>
2024-02-02 15:10:56 -0500

    setting basic structure.

---
+++ app/src/main/java/com/example/notetakingapp/presentation/screens/HomeScreen.kt
@@ -0,0 +1,6 @@
+package com.example.notetakingapp.presentation.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {}
---
+++ app/src/main/java/com/example/notetakingapp/presentation/viewmodel/NoteTakingViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.notetakingapp.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class NoteTakingViewModel(): ViewModel() {
+}
