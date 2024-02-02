c3b36401bc3cb841901be93ba54aeb236863ef79
stefan bayne <stefanbayne@gmail.com>
2024-02-02 15:17:55 -0500

    more app structure.

---
+++ app/src/main/java/com/example/notetakingapp/domain/util/Util.kt
@@ -0,0 +1,2 @@
+package com.example.notetakingapp.domain.util
+
--- app/src/main/java/com/example/notetakingapp/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/example/notetakingapp/presentation/screens/HomeScreen.kt
@@ -1,6 +1,10 @@
 package com.example.notetakingapp.presentation.screens
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.notetakingapp.presentation.viewmodel.NoteTakingViewModel
 
 @Composable
-fun HomeScreen() {}
+fun HomeScreen(
+    noteTakingViewModel: NoteTakingViewModel = viewModel()
+) {}
