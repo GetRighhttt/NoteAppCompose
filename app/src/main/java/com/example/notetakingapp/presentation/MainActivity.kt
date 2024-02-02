0684c0b905ada4d8ae35d0d58512ecc7a52a844f
stefan bayne <stefanbayne@gmail.com>
2024-02-02 18:42:47 -0500

    adding stateflow and emiting values.

--- app/src/main/java/com/example/notetakingapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/notetakingapp/presentation/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.notetakingapp.presentation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.notetakingapp.data.NoteDataSource
 import com.example.notetakingapp.data.model.Note
 import com.example.notetakingapp.presentation.screens.HomeScreen
+import com.example.notetakingapp.presentation.viewmodel.NoteTakingViewModel
 import com.example.notetakingapp.ui.theme.NoteTakingAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +25,8 @@ class MainActivity : ComponentActivity() {
             NoteTakingAppTheme {
                 MyApp {
                     val notes = remember { mutableStateListOf<Note>() }
-                    HomeScreen(
-                        notes = notes,
-                        onAddNote = { notes.add(it)},
-                        onRemoveNote = { notes.remove(it) })
+                    val noteViewModel: NoteTakingViewModel by viewModels()
+                    NotesApp(noteViewModel = noteViewModel)
                 }
             }
         }
@@ -35,6 +38,17 @@ fun MyApp(content: @Composable () -> Unit) {
     NoteTakingAppTheme {
         content()
     }
+}
+
+@SuppressLint("StateFlowValueCalledInComposition")
+@Composable
+fun NotesApp(noteViewModel: NoteTakingViewModel = viewModel()) {
+    val noteList = noteViewModel.noteList.collectAsState().value
+    HomeScreen(
+        notes = noteList,
+        onAddNote = { noteViewModel.addNote(it)},
+        onRemoveNote = { noteViewModel.removeNote(it) })
+
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/notetakingapp/presentation/viewmodel/NoteTakingViewModel.kt
+++ app/src/main/java/com/example/notetakingapp/presentation/viewmodel/NoteTakingViewModel.kt
@@ -1,6 +1,26 @@
 package com.example.notetakingapp.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.notetakingapp.data.NoteDataSource
+import com.example.notetakingapp.data.model.Note
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
-class NoteTakingViewModel(): ViewModel() {
+class NoteTakingViewModel: ViewModel() {
+    private val _noteList = MutableStateFlow<List<Note>>(emptyList())
+    val noteList = _noteList.asStateFlow()
+
+    private val noteDataSource: NoteDataSource = NoteDataSource()
+
+    init {
+        _noteList.value = noteDataSource.loadNotes()
+    }
+    private operator fun MutableStateFlow<List<Note>>.invoke(note: Note) = _noteList.value.plus(note)
+    fun addNote(note: Note) {
+        _noteList.value = _noteList(note)
+    }
+    fun removeNote(note: Note) {
+        _noteList.value = _noteList.value.minus(note)
+    }
+    fun getAllNotes(): List<Note> = _noteList.value
 }
