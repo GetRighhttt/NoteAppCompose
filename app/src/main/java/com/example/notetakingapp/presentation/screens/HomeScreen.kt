3240460458d03152b900c2b109756228a3ffdab3
stefan bayne <stefanbayne@gmail.com>
2024-02-02 16:26:59 -0500

    finished top app bar and bottom bar.

--- app/src/main/java/com/example/notetakingapp/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/example/notetakingapp/presentation/screens/HomeScreen.kt
@@ -1,10 +1,59 @@
 package com.example.notetakingapp.presentation.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.notetakingapp.presentation.viewmodel.NoteTakingViewModel
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.notetakingapp.presentation.widgets.BottomAppBarAbstraction
+import com.example.notetakingapp.presentation.widgets.FloatingActionButtonAbstraction
+import com.example.notetakingapp.presentation.widgets.TopAppBarAbstraction
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomeScreen(
-    noteTakingViewModel: NoteTakingViewModel = viewModel()
-) {}
+//    noteTakingViewModel: NoteTakingViewModel = viewModel()
+) {
+    Scaffold(
+        topBar = {
+            TopAppBarAbstraction()
+        },
+        bottomBar = {
+            BottomAppBarAbstraction(modifier = Modifier.fillMaxWidth())
+        },
+        floatingActionButton = {
+            FloatingActionButtonAbstraction()
+        }
+    ) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                Text(
+                    text = "Home Screen",
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
+}
--- app/src/main/java/com/example/notetakingapp/presentation/widgets/Widgets.kt
+++ app/src/main/java/com/example/notetakingapp/presentation/widgets/Widgets.kt
@@ -1,2 +1,100 @@
 package com.example.notetakingapp.presentation.widgets
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import com.example.notetakingapp.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarAbstraction(modifier: Modifier = Modifier) =
+    CenterAlignedTopAppBar(
+        colors =
+        TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.background,
+            titleContentColor = MaterialTheme.colorScheme.onBackground
+        ),
+        title = {
+            Text(
+                text = stringResource(R.string.note_taking_app),
+                style = MaterialTheme.typography.titleLarge,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = stringResource(R.string.menu)
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = stringResource(R.string.add)
+                )
+            }
+        },
+        modifier = modifier.fillMaxWidth()
+    )
+
+@Composable
+fun BottomAppBarAbstraction(modifier: Modifier = Modifier) =
+    BottomAppBar(
+        actions = {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = stringResource(R.string.home)
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Default.Share,
+                    contentDescription = stringResource(R.string.share)
+                )
+            }
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Default.Settings,
+                    contentDescription = stringResource(R.string.settings)
+                )
+            }
+        }
+    )
+
+@Composable
+fun FloatingActionButtonAbstraction(modifier: Modifier = Modifier) =
+    FloatingActionButton(
+        onClick = { },
+        containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+        elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+    ) {
+        Icon(
+            imageVector = Icons.Default.Favorite,
+            contentDescription = stringResource(R.string.favorite)
+        )
+    }
